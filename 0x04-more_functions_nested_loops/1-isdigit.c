349a3f46a16ae07be523bb70369bbcd2f27665c9
SAFAEE1 <safae.matrafi7@gmail.com>
2023-03-24 07:05:29 -0700

    safae

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,14 +4,13 @@
  * _isdigit - check if numbers are 0 - 9
  * @c: char to be checked
  *
- * Return: 1 or 0
+ * Return: 0 or 1
  */
 
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return (1)
-	else 
-		return (0)		
-
+		return (1);
+	else
+		return (0);
 }
